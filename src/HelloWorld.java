d9640a4b848d7fcc00e111747dc5de63d9d273a2
ocoutolleau <oriane.coutolleau@epita.fr>
2021-05-17 15:51:05 +0200

    add

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Coucou");
+        for (String arg : args){
+            System.out.println(arg);
+        }
+    }
+}
