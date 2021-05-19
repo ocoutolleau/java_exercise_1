823053bfa8c06ea957dde9a3c95e38351b626b4e
ocoutolleau <oriane.coutolleau@epita.fr>
2021-05-19 17:03:54 +0200

    add

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,94 +1,25 @@
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Launcher {
-
     public static void main(String[] args) {
-        System.out.println("coucou");
-        Scanner scanner = new Scanner(System.in);
+        List<Command> commands = List.of(new Quit(), new Fibo());
+        Map<String, Command> commandByName = commands.stream().collect(Collectors.toMap(c -> c.name(), c-> c));
 
+        System.out.println("coucou");
+
+        Scanner scanner = new Scanner(System.in);
         boolean shouldQuit = false;
         do {
             System.out.println("Please, input a command:");
-            String command = scanner.nextLine();
-            if ("fibo".equals(command)) {
-                System.out.println("Input a number:");
-                int n = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println(fibo(n));
-            } else if ("freq".equals(command)) {
-                toto();
-            }else if ("quit".equals(command)) {
-                shouldQuit = true;
-                break;
+            String userInput = scanner.nextLine();
+            Command command = commandByName.get(userInput);
+
+            if (command == null){
+                System.out.println("Unknown command");
+            } else {
+                shouldQuit = command.run(scanner);
             }
-            System.out.println("Unknown command");
         } while (!shouldQuit);
     }
-
-    private static int fibo(int n) {
-        return n == 0 || n == 1 ? n : fibo(n - 1) + fibo(n - 2);
-    }
-    private static void toto(){
-        String[] words = new String[]{"titi", "toto","tata"};
-
-        Stream<String> wordStream = Arrays.stream(words);
-
-        Map<String, Long> countsByWord = wordStream
-                .filter(s -> !s.isBlank())
-                .map(s -> s.toLowerCase())
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-        Comparator<Map.Entry<String, Long>> countReversed =
-                Comparator
-                        .<Map.Entry<String, Long>, Long>comparing(e -> e.getValue())
-                        .reversed();
-
-        String threeMostOccurringWords = countsByWord.entrySet().stream()
-                .sorted(countReversed)
-                .limit(3)
-                .map(e -> e.getKey())
-                .collect(Collectors.joining(" "));
-
-
-        System.out.println((threeMostOccurringWords));
-    }
-
 }
-    /*private static int fibo(int n){
-        if (n == 0 || n== 1) {
-            return n;
-        } else{
-            return fibo(n-1) + fibo(n-2);
-        }
-    }*/
-    /*private static void fibo(int n) {
-        int f0 = 0;
-        int f1 = 1;
-        int f2 = 0;
-        while (n != 1) {
-            f2 = f1 + f0;
-            f0 = f1;
-            f1 = f2;
-        }
-        System.out.println(f2);
-    }*/
-
-
-        /*System.out.println("ton nom");
-        String name = null;
-        try {
-
-            name = scanner.nextLine();
-        }catch (NoSuchElementException e){
-            System.out.println("Nombre svp");
-        }*/
-        //System.out.println("Bonjour" + name);
-
-        /*
-        name.equals("quit") ? continue : System.out.println("Unknown command");
-        */
